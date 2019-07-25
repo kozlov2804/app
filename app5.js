d3384cd23706a03d255ec84a89f2a957620f6a4b
Kozlov2804 <kozlov2804@ukr.net>
2019-07-25 22:04:57 +0300

    fibonachi

--- app5.js
+++ app5.js
@@ -1,5 +1,19 @@
-    for (let i = 0; i <= 20; i++){
-        console.log(i);
-    }   
- 
-    
+/*
+function fibonaci (num){
+    return num <= 2 ? 1 : fibonaci(num-1) + fibonaci(num-2);
+}
+console.log(fibonaci(3));
+*/
+
+function fibonaci(num) {
+    let numbers = [1, 1];
+    for (let i = 2; i < num; i++)
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    return numbers.pop();
+}
+
+
+console.log(fibonaci(8));
+
+// 1 1 2 3 5 8 13 21 34 55 Числа Фибоначчи
+// 1 2 3 4 5 6  7  8  9 10
